1dbdc506d75caaaac5f0435a4edcbf112b2b8363
alejandrofontan <fontanvillacampa@gmail.com>
2025-11-05 17:35:57 +1000

    new calibration yaml file

--- scripts/calibrate.sh
+++ scripts/calibrate.sh
@@ -27,4 +27,4 @@ mkdir -p ${images_folder}
 source devel/setup.bash
 python scripts/vid2imgs.py --video ${video} --output ${images_folder}
 rosrun kalibr kalibr_bagcreater --folder ${output_folder} --output-bag ${output_bag}
-rosrun kalibr kalibr_calibrate_cameras --target src/kalibr/april_6x10.yaml --models pinhole-radtan --topics /cam0/image_raw  --bag ${output_bag}  --bag-freq 24.0 --verbose
+rosrun kalibr kalibr_calibrate_cameras --target scripts/april_10x6.yaml --models pinhole-radtan --topics /cam0/image_raw  --bag ${output_bag}  --bag-freq 24.0 --verbose
